97984e26c7c508a9e8786be0dde063e0fa372cfe
Msksgm <msksgm3124@gmail.com>
2024-01-25 09:10:45 +0900

    build: detekt を導入

--- src/main/kotlin/com/example/patternofkotlinspringbootvalidation/PatternOfKotlinSpringBootValidationApplication.kt
+++ src/main/kotlin/com/example/patternofkotlinspringbootvalidation/PatternOfKotlinSpringBootValidationApplication.kt
@@ -3,9 +3,23 @@ package com.example.patternofkotlinspringbootvalidation
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
+/**
+ * PatternOfKotlinSpringBootValidationApplication
+ *
+ * アプリケーションのエントリーポイント
+ *
+ */
 @SpringBootApplication
 class PatternOfKotlinSpringBootValidationApplication
 
+/**
+ * main
+ *
+ * アプリケーションを起動する際の main 関数
+ *
+ * @param args
+ */
 fun main(args: Array<String>) {
-	runApplication<PatternOfKotlinSpringBootValidationApplication>(*args)
+    @Suppress("SpreadOperator")
+    runApplication<PatternOfKotlinSpringBootValidationApplication>(*args)
 }
